0b3d00c785e0cf3fbc817c6333b3fefc37504171
Marc <mnoris.xeno@gmail.com>
2020-02-26 10:27:52 +0100

    Scaffold + Dossier SQL

---
+++ Models/Etatdossier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bo_Voyage_Final.Models
+{
+    public partial class Etatdossier
+    {
+        public Etatdossier()
+        {
+            Dossierresa = new HashSet<Dossierresa>();
+        }
+
+        public byte Id { get; set; }
+        public string Libelle { get; set; }
+
+        public virtual ICollection<Dossierresa> Dossierresa { get; set; }
+    }
+}
---
+++ Models/Photo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bo_Voyage_Final.Models
+{
+    public partial class Photo
+    {
+        public int Id { get; set; }
+        public string NomFichier { get; set; }
+        public int IdDestination { get; set; }
+
+        public virtual Destination IdDestinationNavigation { get; set; }
+    }
+}
