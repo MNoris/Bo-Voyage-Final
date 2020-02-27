0244e138c762307e54444d4dbf07e0098330eb17
k4ys4r <hilal_balout@hotmail.com>
2020-02-27 14:49:18 +0100

    Area BackOffice

---
+++ Areas/BackOffice/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bo_Voyage_Final.Areas.BackOffice.Controllers
+{
+    [Area("BackOffice")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+    }
+}
--- Areas/Client/Controllers/HomeController.cs
+++ Areas/Client/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Bo_Voyage_Final.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace Bo_Voyage_Final.Areas.Client.Controllers
@@ -14,15 +16,61 @@ namespace Bo_Voyage_Final.Areas.Client.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly BoVoyageContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,BoVoyageContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            /*
+             * "Top 5 Voyages le moins chèrs" = 0
+             * "Top 5 Voyages de départ iminent" )= 1
+             * "Top 5 Voyages pour les pays les plus visités" = 2
+             */
+            var top5Voyages = new Dictionary<int, List<Voyage>>();
+
+            List<Voyage> list5MoinsCher = _context.Voyage.Include(v => v.IdDestinationNavigation)
+                                                         .ThenInclude(d => d.Photo)  
+                                                         .OrderBy(v => v.PrixHt * (1 - v.Reduction)).Take(5).ToList();
+            top5Voyages.Add(0,list5MoinsCher);
+            List<Voyage> list5DepartIminent = _context.Voyage.Include(v => v.IdDestinationNavigation)
+                                                    .ThenInclude(d => d.Photo)
+                                                    .OrderBy(v => v.DateDepart).Take(5).ToList();
+            top5Voyages.Add(1,list5DepartIminent);
+
+
+            List<Voyage> list5VoyagePays = new List<Voyage>();
+            var req = @"select top(5) d.Id,d.Nom,count(*) NbVoyage
+                        from Voyage v
+                        inner join Destination d on d.Id = v.IdDestination
+                        where d.Niveau = 2
+                        group by d.Id,d.Nom
+                        order by count(*) desc
+                        ";
+            using (var cnx = (SqlConnection)_context.Database.GetDbConnection())
+            {
+                var cmd = new SqlCommand(req, cnx);
+                cnx.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read() && list5VoyagePays.Count <6)
+                    {
+                        int idDest = (int)reader["Id"];
+                        var voyages = _context.Voyage.Include(v => v.IdDestinationNavigation)
+                                                     .ThenInclude(d => d.Photo).Where(v => v.IdDestination == idDest)
+                                                     .AsNoTracking().ToList();
+                        list5VoyagePays.AddRange(voyages);
+                    }
+                }
+
+            }
+            top5Voyages.Add(2,list5VoyagePays);
+
+            return View(top5Voyages);
         }
 
         public IActionResult Privacy()
