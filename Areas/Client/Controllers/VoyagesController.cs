a2be3c987912504432f7aa1ff2e38496f6ccdba5
Marc <mnoris.xeno@gmail.com>
2020-02-27 17:02:43 +0100

    Maj des packages

--- Areas/Client/Controllers/VoyagesController.cs
+++ Areas/Client/Controllers/VoyagesController.cs
@@ -31,44 +31,31 @@ namespace Bo_Voyage_Final.Areas.Client.Controllers
 
             //Passer un string format yyyy-MM-dd comme valeur par défaut pour l'input type date
             //valeur par defaut minimal : date du jour, max : 7jours plus tard
-            if (dateMin ==DateTime.MinValue)
-            {
+            if (dateMin == DateTime.MinValue)
                 ViewBag.dateMin = DateTime.Now.ToString("yyyy-MM-dd");
-            }
             else ViewBag.dateMin = dateMin.ToString("yyyy-MM-dd");
 
             if (dateMax == DateTime.MinValue)
-            {
                 ViewBag.dateMax = DateTime.Now.AddDays(7).ToString("yyyy-MM-dd");
-            }
             else ViewBag.dateMax = dateMax.ToString("yyyy-MM-dd");
 
 
 
             //Requete de récupération des voyages et destination
-            IQueryable<Voyage> reqVoyages = _context.Voyage.Include(v => v.IdDestinationNavigation).ThenInclude(e => e.Photo)
+            IQueryable<Voyage> reqVoyages = _context.Voyage.Where(v => v.PlacesDispo > 0).Include(v => v.IdDestinationNavigation).ThenInclude(e => e.Photo)
                 .OrderBy(e => e.IdDestinationNavigation.Nom);
 
             if (IdDestination != 0)
-            {
                 //Application du filtre Sur les destinations
                 reqVoyages = reqVoyages.Where(d => d.IdDestination == IdDestination);
-            }
 
             //filtres par prix
             if (minPrice != 0 || maxPrice != 0)
-            {
-
                 reqVoyages = reqVoyages.Where(p => p.PrixHt <= maxPrice && p.PrixHt >= minPrice);
-            }
 
             //filtres par date de départ
             if (dateMin != DateTime.MinValue || dateMax != DateTime.MinValue)
-            {
                 reqVoyages = reqVoyages.Where(d => d.DateDepart >= dateMin && d.DateDepart <= dateMax);
-            }
-
-
 
             var listeVoyages = await reqVoyages.AsNoTracking().ToListAsync();
 
@@ -79,22 +66,45 @@ namespace Bo_Voyage_Final.Areas.Client.Controllers
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
-            {
                 return NotFound();
-            }
 
             var voyage = await _context.Voyage
                 .Include(v => v.IdDestinationNavigation).ThenInclude(d => d.Photo)
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (voyage == null)
-            {
                 return NotFound();
-            }
 
             return View(voyage);
         }
 
+        public async Task<IActionResult> Reserver(int? id)
+        {
+            //    string mail = "";
+            //    if (!string.IsNullOrWhiteSpace((string)ViewBag.User))
 
+
+
+            try
+            {
+                var mail = (string)ViewBag.User;
+                var user = await _context.Personne.AsNoTracking().FirstOrDefaultAsync(p => p.Email == mail);
+                var voyage = await _context.Voyage.Include(v => v.IdDestinationNavigation).FirstOrDefaultAsync(v => v.Id == id);
+
+                var personneVoyage = new PersonneVoyage(user, voyage);
+
+                return View("Reserver", personneVoyage);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+
+
+            throw new NotImplementedException();
+        }
 
         private bool VoyageExists(int id)
         {
---
+++ Models/PersonneVoyage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bo_Voyage_Final.Models
+{
+    public class PersonneVoyage
+    {
+        public PersonneVoyage()
+        {
+
+        }
+
+        public PersonneVoyage(Personne personnes, Voyage voyages)
+        {
+            Personne = personnes;
+            Voyage = voyages;
+        }
+
+        public Personne Personne { get; set; }
+        public Voyage Voyage { get; set; }
+    }
+}
