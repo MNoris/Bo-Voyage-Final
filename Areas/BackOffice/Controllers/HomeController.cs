f426bbf0f64f50d81fd6148961dea19056083c40
Thomas <thomas.mouton@ymail.com>
2020-02-27 16:55:57 +0100

    Debut du backOffice (viewModel + tableau de bord)

--- Areas/BackOffice/Controllers/HomeController.cs
+++ Areas/BackOffice/Controllers/HomeController.cs
@@ -2,18 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bo_Voyage_Final.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bo_Voyage_Final.Areas.BackOffice.Controllers
 {
     [Area("BackOffice")]
     public class HomeController : Controller
     {
+        private readonly BoVoyageContext _context;
+            public HomeController (BoVoyageContext context)
+            {
+                _context = context;
+            }
         public IActionResult Index()
         {
+            var DateLimite = DateTime.Now.AddDays(15);
 
 
-            return View();
+
+            List<Voyage>voyages = _context.Voyage.Include(d=>d.IdDestinationNavigation).ThenInclude(p=>p.Photo)
+                                                                    .Where(i=>i.DateDepart<= DateLimite)
+                                                                    .AsNoTracking().ToList();
+
+
+
+           List<Dossierresa> reservations = _context.Dossierresa.Include(d=>d.IdEtatDossierNavigation)
+              .Where(d=>d.IdEtatDossierNavigation.Id==1).AsNoTracking().ToList();
+
+
+            VoyagesDossiersResa vdr = new VoyagesDossiersResa(voyages, reservations);
+
+
+
+            return View(vdr);
         }
     }
 }
---
+++ Models/VoyagesDossiersResa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bo_Voyage_Final.Models
+{
+    public class VoyagesDossiersResa
+    {
+
+        public List<Voyage> listeVoyageImminent { get; set; }
+
+        public  List<Dossierresa> listeDossierResaEnCours { get; set; }
+
+        public VoyagesDossiersResa(List<Voyage> listeVoyageImminent, List<Dossierresa> listeDossierResaEnCours)
+        {
+            this.listeVoyageImminent = listeVoyageImminent;
+            this.listeDossierResaEnCours = listeDossierResaEnCours;
+        }
+    }
+}
