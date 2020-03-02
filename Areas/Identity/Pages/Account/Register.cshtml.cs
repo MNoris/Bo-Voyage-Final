176ef1a400c7748bc0ee99b433c5d203b0013a63
Mnoris <mnoris.xeno@gmail.com>
2020-03-02 16:55:15 +0100

    Ajout création personne en même temps utilisateur

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using Bo_Voyage_Final.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -23,17 +24,20 @@ namespace Bo_Voyage_Final.Areas.Identity.Pages.Account
         private readonly UserManager<IdentityUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly BoVoyageContext _context;
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            BoVoyageContext context)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _context = context;
         }
 
         [BindProperty]
@@ -60,6 +64,17 @@ namespace Bo_Voyage_Final.Areas.Identity.Pages.Account
             [Display(Name = "Confirmation du mot de passe")]
             [Compare("Password", ErrorMessage = "Le mot de passe et sa confirmation ne correspondent pas.")]
             public string ConfirmPassword { get; set; }
+
+            [Display(Name = "Civilité")]
+            public string Civilite { get; set; }
+            [Display(Name = "Nom")]
+            public string Nom { get; set; }
+            [Display(Name = "Prénom")]
+            public string Prenom { get; set; }
+            [Display(Name = "Numéro de téléphone"), DataType(DataType.PhoneNumber)]
+            public string Telephone { get; set; }
+            [Display(Name = "Date de Naissance"), DataType(DataType.Date)]
+            public DateTime? Datenaissance { get; set; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
@@ -74,11 +89,29 @@ namespace Bo_Voyage_Final.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
+                //Partie compte Personne
+                var personne = new Personne 
+                { Email = Input.Email, Nom = Input.Nom, Prenom = Input.Prenom, Civilite = Input.Civilite, 
+                    TypePers = 2, Telephone = Input.Telephone, Datenaissance = Input.Datenaissance};
+
+                try
+                {
+                    _context.Personne.Add(personne);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+                //Partie standard compte AspNetUser
                 var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
+
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("L'utilisateur a créer un nouveau compte avec mot de passe.");
+                    _logger.LogInformation("L'utilisateur a créé un nouveau compte avec mot de passe.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
@@ -88,7 +121,7 @@ namespace Bo_Voyage_Final.Areas.Identity.Pages.Account
                         values: new { area = "Identity", userId = user.Id, code = code },
                         protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    await _emailSender.SendEmailAsync(Input.Email, "Confirmer votre email",
                         $"Confirmez votre compte en <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>cliquant ici</a>.");
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
